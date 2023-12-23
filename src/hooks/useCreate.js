126ca7b607f220c39428f36b275c6fbd78a702de
Blaise <blaisebhg3828@gmail.com>
2023-12-23 17:01:59 +0200

    creating useCreate hook

---
+++ src/hooks/useCreate.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import AxiosInstance from '../axios/AxiosInstance';
+
+const useCreate =  () => {
+    const [load,setLoad]=useState(false) 
+    const [error,setError]=useState(false)  
+    const [success,setSuccess]=useState(false)
+    const create=(url,data)=>{
+        setLoad(true)
+        AxiosInstance.post(url,data).then((result) => {
+            if(result.status){
+                setSuccess(true)
+            }
+        }).catch((err) => {
+            console.log(err);
+            if(err){
+                setError(true)
+            }
+        }); 
+        setTimeout(() => {
+            setLoad(false)
+            setSuccess(false)
+            setError(false)
+        }, 4000);
+    }
+   
+
+    return { create, load, error, success };
+};
+
+export default useCreate;
