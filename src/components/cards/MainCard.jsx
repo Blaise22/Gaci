2cbe4b622e206fb24c11b5a50141798fe5f9e20b
Blaise <blaisebhg3828@gmail.com>
2023-12-23 17:07:21 +0200

    creating main card

---
+++ src/components/cards/MainCard.jsx
@@ -0,0 +1,21 @@
+import { HomeModernIcon } from '@heroicons/react/20/solid'
+import React from 'react'
+
+const MainCard = ({children,className, mainTitle, mainIcon,sideHeaderContent}) => {
+  return (
+    <div className={className}>
+         <div className="p-2 flex justify-between border-b">
+            <div className="flex gap-2 items-center">
+                {mainIcon}
+                <span className="text-secondary-4 font-bold"> {mainTitle}</span>
+            </div>
+            {sideHeaderContent}
+         </div>
+         <div className="p-4 flex flex-col gap-4">
+            {children}
+         </div>
+      </div>
+  )
+}
+
+export default MainCard
