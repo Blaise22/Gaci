f95dc41f84613a7eed0bdfb7852f8eac02778bde
Blaise <blaisebhg3828@gmail.com>
2023-12-26 14:59:50 +0200

    importing discussion card for responses

--- src/components/cards/CardDiscussion.jsx
+++ src/components/cards/CardDiscussion.jsx
@@ -1,49 +1,36 @@
 import React, { useContext } from 'react'
 import {  QuestionMarkCircleIcon,ChatBubbleBottomCenterIcon,TrashIcon} from '@heroicons/react/24/outline'
-import { UserCircleIcon} from '@heroicons/react/24/solid'
-import { AuthContext } from '../../context/AuthContext'
-import AxiosInstance from '../../api/axios'
-import useFetch from '../../hooks/useFetch'
-import getPeriode from '../../helpers/utils/getPeriode'
-import DeletModale from '../modals/DeletModale'
+import { UserCircleIcon} from '@heroicons/react/24/solid' 
 const CardDiscussion = ({message,date,owner,id,refresh}) => {
-    const {user}=useContext(AuthContext)
-    const {data,loading}=useFetch('auth/users/'+owner)
+     
+ 
      
   return (
-    <div className="block w-auto mt-1 border-b pb-3 text-sm">
+    <div className="block md:text-sm w-auto mt-1 border-b pb-3 text-xs">
                 <div className="flex group justify-between">
                     <div className="flex items-center gap-2">
-                        {
-                            data?.profil?
-                            <img src={data?.profil.pic} className='w-10 h-10 object-cover bg-gray-200 rounded-full'  alt="" />
-                            : <UserCircleIcon className='w-10 text-gray-600' />
+                         <UserCircleIcon className='w-10 text-gray-600' />
  
-                        }
-                        <span className="text-secondary-2 font-bold capitalize">{ data?.first_name} {data?.last_name}</span>
+                        
+                        <span className="text-secondary-2 font-bold capitalize">Blaise nhg</span>
                     </div>
                     <div className="flex items-center gap-2">
                          
                         <div className="flex gap-2 items-center z-0 cursor-pointer w-10 justify-center rounded-md"> 
-                            {
-                                owner && user &&
-                                <div  className={owner==user?.id?"block":"hidden"} >
-                                    <DeletModale endPoint={`courses/video-comments/${id}`} refresh={refresh} />
-                                </div> 
-                            }
+                             Delete
                         </div>
                          
                     </div> 
                 </div>
-                <div className={owner==user?.id?"text-violet-600":"text-gray-600"}>
+                <div className={"text-gray-600"}>
                       
-                        {message}
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ab harum modi tempore illum aliquid officia quod odit. Neque provident vitae eius accusantium tempore incidunt architecto harum laboriosam ex delectus!
                                         
                 </div>
                  
                 <div className="flex mt-2 items-center justify-end gap-2">
                      
-                    <span className="text-gray-700 text-xs">Publié { getPeriode(date) }</span> 
+                    <span className="text-gray-700 text-xs">Publié il ya deux min</span> 
                 </div>
             </div>
   )
--- src/pages/forum/Responses.jsx
+++ src/pages/forum/Responses.jsx
@@ -1,12 +1,54 @@
 import React from 'react'
 import Header from '../../components/partials/Header'
+import {QuestionMarkCircleIcon,PlusIcon} from '@heroicons/react/20/solid'
+import MainCard from '../../components/cards/MainCard'
+import CardDiscussion from '../../components/cards/CardDiscussion'
 
 const Responses = () => {
   return (
     <>
         <Header/>
         <div className="pt-16 px-6 text-xs md:text-sm text-gray-700  w-full">
-            Hello
+        <div className=" py-2 md:px-14 lg:px-24 "> 
+                <span className="text-blue-700 font-semibold">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat quae illum nobis a, dolorum, quaerat sint sed, cum commodi obcaecati corrupti nulla corporis consectetur dolores esse fuga ea deserunt non?
+                </span>
+                <p className="text-gray-700 text-xs block mt-2">
+                    Publiéil ya 20 min
+
+                </p>
+             <MainCard
+                className={'bg-white rounded-lg w-full'}
+                mainIcon={<QuestionMarkCircleIcon className='w-8 text-gray-700' />}
+                mainTitle={'Reponses'}
+             >
+              <div className="flex pb-16 gap-4 flex-col">
+                <CardDiscussion  />
+                <CardDiscussion  />
+                <CardDiscussion  />
+                <CardDiscussion  />
+                <CardDiscussion  />
+                <CardDiscussion  />
+                <CardDiscussion  />
+                <CardDiscussion  />
+                <CardDiscussion  />
+                <CardDiscussion  />
+                <CardDiscussion  />
+                <CardDiscussion  />
+                <CardDiscussion  />
+                <CardDiscussion  />
+                <CardDiscussion  />
+                <CardDiscussion  />
+                <CardDiscussion  />
+
+              </div>
+
+
+            </MainCard>
+        </div>
+        <div className="fixed w-full py-2 md:px-14 lg:px-24 px-6 bg-white h-20 border-t bottom-0 right-0 left-0">
+            hello
+        </div>
         </div>
     </>
   )
