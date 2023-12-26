24e45c86949d76a344fb2b9a9a685d1366067460
Blaise <blaisebhg3828@gmail.com>
2023-12-26 10:16:55 +0200

    add question link on forum sidebar

--- src/components/partials/ForumSidebar.jsx
+++ src/components/partials/ForumSidebar.jsx
@@ -1,15 +1,29 @@
 import React, { useState } from 'react'
-import {Bars3Icon} from '@heroicons/react/20/solid'
+import {ChevronRightIcon,XMarkIcon,QuestionMarkCircleIcon} from '@heroicons/react/20/solid'
+import { Link } from 'react-router-dom'
 const ForumSidebar = () => {
-    const [isOpen,setIsOpen]=useState(true)
+    const [isOpen,setIsOpen]=useState(false)
     return (
         <>
             {
-                !isOpen && <Bars3Icon onClick={()=>{setIsOpen(true)}} className={'icon-primary fixed left-4 top-16'}/>
+                !isOpen && <ChevronRightIcon onClick={()=>{setIsOpen(true)}} className={'icon-primary fixed left-4 top-16'}/>
             }
-            <div className={`w-64 border-r h-full p-2 fixed z-50 lelft-0 top-14 ${!isOpen?'left-[-100%] tran':'left-0 tran'}`}>
+            <div className={`w-64 border-r bg-white h-full bottom-0 p-2 fixed z-50 lelft-0 top-14 ${!isOpen?'left-[-100%] tran':'left-0 tran'}`}>
                 <div className="flex justify-end">
-                    <Bars3Icon onClick={()=>{setIsOpen(false)}} className={'icon-danger'}/> 
+                    
+                    <XMarkIcon onClick={()=>{setIsOpen(false)}} className={'icon-danger'}/> 
+                </div>
+                <div className="flex flex-col gap-4">
+                    <Link to={'/forum/questions'} className='group' >
+                        <span className="text-lg text-gray-700 font-bold group-hover:bg-blue-600 group-hover:text-white tran p-2 rounded-lg flex gap-1 items-center block">
+                            <QuestionMarkCircleIcon className='w-10'/>
+                            Questions
+                        </span>
+                    </Link>
+                    <span className="text-blue-600 text-sm p-4">
+                    4000 Questions posees   
+                    </span>
+
                 </div>
             </div>
         </>
