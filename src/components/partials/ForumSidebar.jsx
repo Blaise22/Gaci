3a4d6ac5ac84ac8bb369a87b763e93922c6086ff
Blaise <blaisebhg3828@gmail.com>
2023-12-26 09:51:14 +0200

    toggle forum sidebar

--- src/components/partials/ForumSidebar.jsx
+++ src/components/partials/ForumSidebar.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {Bars3Icon} from '@heroicons/react/20/solid'
 const ForumSidebar = () => {
-  return (
-    <div>ForumSidebar</div>
+    const [isOpen,setIsOpen]=useState(true)
+    return (
+        <>
+            {
+                !isOpen && <Bars3Icon onClick={()=>{setIsOpen(true)}} className={'icon-primary fixed left-4 top-16'}/>
+            }
+            <div className={`w-64 border-r h-full p-2 fixed z-50 lelft-0 top-14 ${!isOpen?'left-[-100%] tran':'left-0 tran'}`}>
+                <div className="flex justify-end">
+                    <Bars3Icon onClick={()=>{setIsOpen(false)}} className={'icon-danger'}/> 
+                </div>
+            </div>
+        </>
   )
 }
 
--- src/pages/forum/Forum.jsx
+++ src/pages/forum/Forum.jsx
@@ -1,11 +1,13 @@
 
 import React from 'react'
 import Header from '../../components/partials/Header'
+import ForumSidebar from '../../components/partials/ForumSidebar'
 
 const Forum = () => {
   return (
     <>
         <Header/>
+        <ForumSidebar />
         <div className='px-4 md:px-12 lg:px-16 w-full'>
             
         </div>
