7a1200d61edfc92975954b854bdaa3edc2a8c32c
Blaise <blaisebhg3828@gmail.com>
2023-12-31 13:42:25 +0200

    create a like on project

--- src/components/cards/CardProject.jsx
+++ src/components/cards/CardProject.jsx
@@ -1,14 +1,35 @@
 import Thumbail from '../../assets/img.jpg' 
 import getPeriode from '../../helpers/utils/getPeriode'
-import {QuestionMarkCircleIcon,EllipsisHorizontalIcon} from '@heroicons/react/20/solid'
+import {QuestionMarkCircleIcon,EllipsisHorizontalIcon,HandThumbUpIcon} from '@heroicons/react/20/solid'
 import {ChatBubbleLeftIcon,EyeIcon} from '@heroicons/react/24/outline'
 import DeletModale from '../modals/DeletModale'
 import useUser from '../../hooks/useUser'
 import { Link,useNavigate } from 'react-router-dom'
 import useCreate from '../../hooks/useCreate'
+import useFetch from '../../hooks/useFetch'
+import useFetchPaginate from '../../hooks/useFetchPaginate'
+import abbreviateNumber from '../../helpers/utils/abbreviateNumber'
 const CardProject = ({date,description,designation,dev,doc,image,pu,pk,pub,user:owner,refresh}) => {
+    const {data:likes,load,error,getData:getLikes}=useFetchPaginate(`/pub/like-project-project-list/${pk}`)
+    const {create,res,success,error:errorCreateLik}=useCreate()
     const user=useUser()
-  return (
+    
+    
+    const handleLike=(pk)=>{
+        if(likes.filter(item=> item.user.pk==user?.user.pk)?.length>0){
+            // delete
+            console.log('delete ',pk);
+        }else{
+            //create
+            const formData={
+                project_id:!load ? pk : null,
+                status:true
+            }
+            create('/pub/like-project-list-create/',formData)
+        }
+        getLikes(`/pub/like-project-project-list/${pk}`)
+    }
+    return (
     <>
         <div className="w-full flex shadow-md rounded-lg flex-col">
           
@@ -46,10 +67,19 @@ const CardProject = ({date,description,designation,dev,doc,image,pu,pk,pub,user:
                                 }
                                 
                             </div>
-              <h1 className='text-gray-700 block pt-4 pb-2 block border-t text-md font-semibold flex flex-col'>Développeur <span className='text-sm font-normal'>{dev}</span></h1>
-                <h1 className='text-blue-600 text-lg font-semibold '>{designation}</h1> 
-                <h1 className='text-gray-700 text-md flex flex-col font-semibold blck mt-2'>Prix unitaire<span className='text-sm font-normal'>{pu}</span></h1> 
+              <h1 className='text-gray-700 block pt-4 px-1.5 pb-2 block border-t text-md font-semibold flex flex-col'>Développeur <span className='text-sm font-normal'>{dev}</span></h1>
+                <h1 className='text-blue-600 px-1.5 text-lg font-semibold '>{designation}</h1> 
+                <h1 className='text-gray-700 px-1.5 text-md flex flex-col font-semibold blck mt-2'>Prix unitaire<span className='text-sm font-normal'>{pu}</span></h1> 
+                      
+                      <div className={`mt-2 rounded-full inline  p-1.5 flex items-center gap-2 ${likes.filter(item=> item.user.pk==user?.user.pk)?.length>0?'text-blue-600 ':'text-gray-600 '} `}>
+                          <HandThumbUpIcon onClick={()=>{handleLike(pk)}} className='w-8 p-0.5 active:border rounded-full tran cursor-pointer' />
+                          {
+                              !load &&
+                              <span>{abbreviateNumber(likes?.length)} j'aimes</span>
+                          }
+                      </div>
                 <div className="flex w-full items-center mt-2 justify-between">
+
                   <div className="flex gap-2 items-center"> 
                       <Link to={'/projet/'+pk} className='hover:bg-blue-100 rounded-lg tran hover:text-blue-600 p-1.5 text-gray-700 group flex items-center gap-2'>
                           <EyeIcon className='w-6' />
