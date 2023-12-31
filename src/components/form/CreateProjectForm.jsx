0a69c8dd827639741b4ffabf83bbae663aa5d5d1
Blaise <blaisebhg3828@gmail.com>
2023-12-31 12:12:44 +0200

    handle create project

--- src/components/form/CreateProjectForm.jsx
+++ src/components/form/CreateProjectForm.jsx
@@ -7,15 +7,17 @@ import {useNavigate} from 'react-router-dom'
 import {BookOpenIcon,CameraIcon} from '@heroicons/react/24/outline'
 const CreateProjectForm = ({onClose}) => {
   const [formError,setFormError]=useState(null)
-  const [title,settitle]=useState(null)
-  const [synthesis,setsynthesis]=useState(null)
-  const [text,settext]=useState(null)
-  const [conclusion,setconclusion]=useState(null)
+  const [designationProject,setdesignationProject]=useState(null)
+  const [descriptionProject,setdescriptionProject]=useState(null)
+  const [pu,setpu]=useState(null)
+  const [pub,setpub]=useState(null)
+  const [devInfos,setdevInfos]=useState(null)
   const [image,setImage]=useState(null) 
+  const [doc,setDoc]=useState(null) 
   const {create,res,load, error, success}=useCreate()
   const navigate=useNavigate()
   useEffect(() => {
-    res?.pk && navigate('/publications')
+    res?.pk && navigate('/projets')
     if(success){
       onClose()
     }
@@ -32,26 +34,44 @@ const CreateProjectForm = ({onClose}) => {
     reader.readAsDataURL(file);
   };
    
+  const handleDocChange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+        setDoc(reader.result);
+    };
+
+    reader.readAsDataURL(file);
+  };
  
   const submit=()=>{ 
     const formData={
-      title:title,
-      synthesis:synthesis,
-      image64:image, 
-      text:text,
-      conclusion:conclusion,
-      repost:false,
-      status: true
+      designationProject: designationProject,
+      descriptionProject: descriptionProject,
+      pu: pu,
+      pub:pub,
+      devInfos: devInfos,
+      image64: image,
+      doc64: doc
     }
      
-    if(title){
-      create(`pub/post-list-create/`,formData)
+    if(designationProject && descriptionProject){
+      if(pu){
+        if(devInfos){
+          create(`pub/project-list-create/`,formData)
+        }else{
+          setFormError('Veuillez renseigner les informationsur le develeppeur')
+        }
+      }else{
+        setFormError('Veuillez decrire le prix de votre projet')
+      }
     }else{
-      setFormError('Veuillez decrire votre publication')
-      setTimeout(() => {
-        setFormError(null)
-      }, 4000);
+      setFormError('Veuillez decrire votre projet')
     }
+    setTimeout(() => {
+      setFormError(null)
+    }, 4000);
   }
   
  
@@ -65,38 +85,51 @@ const CreateProjectForm = ({onClose}) => {
        <div className="text-red-600 text-sm">{formError}</div> 
       
        
-       
-      <Input 
+       <Input 
         type={'text'}
         placeholder={''}
-        name={'title'}
-        onChange={settitle}
-        value={title}
-        label={'Titre'}
+        name={'designationProject'}
+        onChange={setdesignationProject}
+        value={designationProject}
+        label={'Designation'} 
+      />
+      <Input 
+        type={'textarea'} 
+        name={'descriptionProject'}
+        onChange={setdescriptionProject}
+        value={descriptionProject}
+        placeholder={'Décrivez votre projet ...'}
+        
         
 
       />
       <Input 
-        type={'textarea'}
-        placeholder={'Entrez la synthese de la publication ...'}
-        name={'synthesis'}
-        onChange={setsynthesis}
-        value={synthesis}
+        type={'text'}
+        placeholder={''}
+        name={'pu'}
+        onChange={setpu}
+        value={pu}
+        label={'Prix'} 
       />
       <Input 
-        type={'textarea'}
-        placeholder={'Entrez les details de la publication ...'}
-        name={'text'}
-        onChange={settext}
-        value={text}
+        type={'text'}
+        placeholder={''}
+        name={'pub'}
+        onChange={setpub}
+        value={pub}
+        label={'Pub'} 
       />
       <Input 
-        type={'textarea'}
-        placeholder={'Entrez la conclusion de la publication ...'}
-        name={'conclusion'}
-        onChange={setconclusion}
-        value={conclusion}
+        type={'text'}
+        placeholder={''}
+        name={'devInfos'}
+        onChange={setdevInfos}
+        value={devInfos}
+        label={'Développeur'} 
       />
+       
+       
+      
        
 
 
@@ -112,7 +145,17 @@ const CreateProjectForm = ({onClose}) => {
               </div>
 
             </div>
-             
+            <div className="flex flex-col w-full">
+                <label htmlFor="" className='text-xs'>Document</label>
+                <div className={`rounded-md border border-blue-500 bg-gray-50 p-1 hover:bg-blue-200 tran w-10 ${doc && 'bg-blue-200'}`}>
+                  <label htmlFor="uploadDoc" className="flex flex-col items-center gap-2 cursor-pointer">
+                    <BookOpenIcon className='w-6 text-blue-600' />
+                     
+                  </label>
+                  <input id="uploadDoc" type="file" onChange={handleDocChange} className="hidden" />
+              </div>
+                 
+            </div>
         </div>
         <Button
         className={'btn-primary mt-4'}
