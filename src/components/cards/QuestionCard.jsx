e9c0d066f0eab72b193fd3ab8028f22cd1ac8f0a
Blaise <blaisebhg3828@gmail.com>
2024-01-03 12:19:53 +0200

    fetch responses in question card

--- src/components/cards/QuestionCard.jsx
+++ src/components/cards/QuestionCard.jsx
@@ -8,7 +8,7 @@ import getPeriod from '../../helpers/utils/getPeriode'
 const QuestionCard = ({refresh,owner,message,dateAdd,dateUpdate,image,doc,pk}) => {
     const navigate=useNavigate() 
     const user=useUser() 
-    const {data,load}=useFetch(`forum/reply-list/`)
+    const {data,load}=useFetch(`forum/reply-list/${pk}`)
      
     return (
     <div className='shadow-md  p-2 md:p-4 rounded-lg'>
@@ -22,7 +22,7 @@ const QuestionCard = ({refresh,owner,message,dateAdd,dateUpdate,image,doc,pk}) =
             </div>
             <div className={`flex gap-2 items-center justify-between  pt-2 mt-2 `}> 
                 <div className='flex flex-col'>
-                    <span className='text-blue-600 font-bold'>{data?.count} reponses </span>
+                    <span className='text-blue-600 font-bold'>{data?.count} reponse {data?.count>1?'s':''} </span>
                     <span className='text-xs text-gray-600 '>Publiée {getPeriod(dateAdd)}</span>
                 </div>
                 <button onClick={()=>{navigate('/forum/question/'+pk+'/reponses/')}} className='btn-primary flex items-center gap-2'>
