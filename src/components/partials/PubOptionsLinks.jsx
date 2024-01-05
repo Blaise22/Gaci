c55372bedb94df24404b97557246e2d1b9c9ad63
Blaise <blaisebhg3828@gmail.com>
2024-01-05 11:56:38 +0200

    add back button on pub options links card

--- src/components/partials/PubOptionsLinks.jsx
+++ src/components/partials/PubOptionsLinks.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import {PlayCircleIcon,PhotoIcon,XMarkIcon,FolderOpenIcon,DocumentIcon} from '@heroicons/react/24/outline'
+import {PlayCircleIcon,PhotoIcon,XMarkIcon,FolderOpenIcon,DocumentIcon,ChevronLeftIcon} from '@heroicons/react/24/outline'
 import { useParams,useNavigate } from 'react-router-dom'
 
 const PubOptionsLinks = ({postId}) => {
@@ -18,6 +18,10 @@ const PubOptionsLinks = ({postId}) => {
             
              
              
+            <div onClick={()=>{changePage(`/publication/${postId}`)}} className="flex cursor-pointer items-center p-2 hover:bg-blue-100  tran hover:text-blue-700 rounded-full gap-2">
+                <ChevronLeftIcon className='w-5' /> 
+                 
+            </div>
             <div onClick={()=>{changePage(`/publication/${postId}/photos`)}} className="flex cursor-pointer items-center p-2 hover:bg-blue-100  tran hover:text-blue-700 rounded-full gap-2">
                 <PhotoIcon className='w-5' /> 
                 <span className='text-md font-bold'>Photos</span>
