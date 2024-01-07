675a41da679ef7ab7deedb4e6043dd0186c8a414
Blaise <blaisebhg3828@gmail.com>
2024-01-07 17:27:21 +0200

    record video

--- src/components/form/CreateVideoForm.jsx
+++ src/components/form/CreateVideoForm.jsx
@@ -4,15 +4,16 @@ import Button from './Button'
 import PopUp from './PopUp'
 import useCreate from '../../hooks/useCreate'
 import {useNavigate} from 'react-router-dom'
-import {BookOpenIcon,CameraIcon} from '@heroicons/react/24/outline'
+import {BookOpenIcon,VideoCameraIcon} from '@heroicons/react/24/outline'
 const CreateVideoForm = ({onClose,postId,onCreate}) => {
   const [formError,setFormError]=useState(null)
   const [wording,setWording]=useState(null) 
-  const [image,setImage]=useState(null)
+  const [vid,setVideo]=useState(null)
   const [filename,setFilename]=useState(null)
-  const {create,res,load, error, success}=useCreate()
+  const {create,res,load, error,errorDetails, success}=useCreate()
   const navigate=useNavigate()
   useEffect(() => {
+    console.log(errorDetails);
     success && onCreate()
     if(success){
       onClose()
@@ -20,12 +21,12 @@ const CreateVideoForm = ({onClose,postId,onCreate}) => {
   }, [error, success,res]) 
   
    
-  const handleImageChange = (event) => {
+  const handleVideoChange = (event) => {
     const file = event.target.files[0];
     const reader = new FileReader();
     setFilename(file.name);
     reader.onload = () => {
-        setImage(reader.result);
+        //setVideo(reader.result);
     };
 
     reader.readAsDataURL(file);
@@ -35,13 +36,13 @@ const CreateVideoForm = ({onClose,postId,onCreate}) => {
     const formData={
       wording:wording, 
       post_id:postId, 
-      images64:image
+      url:vid
     }
      
-    if(image){
-      create(`/pub/post-images-list-create/`,formData)
+    if(vid){
+      create(`/pub/post-video-list-create/`,formData)
     }else{
-      setFormError('Veuillez selectionner une image')
+      setFormError('Veuillez selectionner une video')
       setTimeout(() => {
         setFormError(null)
       }, 4000);
@@ -53,8 +54,8 @@ const CreateVideoForm = ({onClose,postId,onCreate}) => {
     <div className='flex flex-col'>
       <PopUp
         load={load}
-        errorMessage={error?'Veuillez decrire la photo':null}
-        successMessage={success?'Le document a été posté':null}
+        errorMessage={error?'Veuillez decrire la video':null}
+        successMessage={success?'La video a été posté':null}
        />
        <div className="text-red-600 text-sm">{formError}</div>
       <Input 
@@ -65,23 +66,34 @@ const CreateVideoForm = ({onClose,postId,onCreate}) => {
         label={'Description'}
 
       />
+      <Input 
+        type={'text'} 
+        name={'url'}
+        onChange={setVideo}
+        value={vid}
+        label={'URL de la video'}
+
+      />
        
 
 
       <div className="flex  items-center justify-between">
+        {
+          /**
         <div className="flex gap-2"> 
             <div className="flex flex-col w-full">
                 <label htmlFor="" className='text-xs text-blue-600'>{filename}</label>
-                <label htmlFor="" className='text-xs'>Image</label>
-                <div className={`rounded-md border border-blue-500 bg-gray-50 p-1 hover:bg-blue-200 tran w-10 ${image && 'bg-blue-200'}`}>
+                <label htmlFor="" className='text-xs'>Video</label>
+                <div className={`rounded-md border border-blue-500 bg-gray-50 p-1 hover:bg-blue-200 tran w-10 ${vid && 'bg-blue-200'}`}>
                   <label htmlFor="upload" className="flex flex-col items-center gap-2 cursor-pointer">
-                    <CameraIcon className='w-6 text-blue-600' /> 
+                    <VideoCameraIcon className='w-6 text-blue-600' /> 
                   </label>
-                  <input id="upload" type="file" onChange={handleImageChange} className="hidden" />
+                  <input id="upload" type="file" onChange={handleVideoChange} className="hidden" />
               </div>
                  
             </div>
-        </div>
+        </div> */
+        }
         <Button
         className={'btn-primary mt-2 font-bold'}
         click={submit}
