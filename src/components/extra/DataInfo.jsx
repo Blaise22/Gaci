c7f529d3644a79d181527732d07e7dfde80dee02
Blaise <blaisebhg3828@gmail.com>
2024-01-07 18:33:37 +0200

    remove border on data info container

--- src/components/extra/DataInfo.jsx
+++ src/components/extra/DataInfo.jsx
@@ -6,13 +6,13 @@ const DataInfo = ({errorStatus,len,load,message}) => {
         {
             !load &&
             errorStatus ?
-                <div className="flex justify-center py-12 w-full border rounded lg bg-gray-100">
+                <div className="flex justify-center py-12 w-full  rounded lg bg-gray-100">
                     Une erreur est survenue
                 </div>
             :
             !load &&
             len==0?
-                <div className="flex justify-center py-12 w-full border rounded lg bg-gray-100">
+                <div className="flex justify-center py-12 w-full  rounded lg bg-gray-100">
                     {message?message:"Aucun resultat"}
                 </div>
             :null
