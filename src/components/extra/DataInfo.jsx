c2448e297bfdc9d3c0e997142e9bead72f82dcd0
Blaise <blaisebhg3828@gmail.com>
2024-01-07 18:43:47 +0200

    add exclamation icon on data info container

--- src/components/extra/DataInfo.jsx
+++ src/components/extra/DataInfo.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
-
+import {ExclamationTriangleIcon} from '@heroicons/react/20/solid'
 const DataInfo = ({errorStatus,len,load,message}) => {
   return (
     <>
         {
             !load &&
             errorStatus ?
-                <div className="flex justify-center py-12 w-full  rounded lg bg-gray-100">
+                <div className="flex justify-center py-12 w-full flex-col items-center  rounded lg bg-gray-100">
+                    <ExclamationTriangleIcon className='w-12 md:w-20 text-gray-500' />
                     <span className='text-gray-600'>Une erreur est survenue</span>
                 </div>
             :
             !load &&
             len==0?
-                <div className="flex justify-center py-12 w-full  rounded lg bg-gray-100">
+                <div className="flex justify-center py-12 w-full flex-col items-center  rounded lg bg-gray-100">
+                    <ExclamationTriangleIcon className='w-12 md:w-20 text-gray-500' />
                     <span className='text-gray-600'>{message?message:"Aucun resultat"}</span>
                 </div>
             :null
