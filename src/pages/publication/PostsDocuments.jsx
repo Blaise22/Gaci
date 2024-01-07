e2558be347ff584ab7dda0f813af716b04258273
Blaise <blaisebhg3828@gmail.com>
2024-01-07 18:11:37 +0200

    post documents emptty info-corrections

--- src/pages/publication/PostsDocuments.jsx
+++ src/pages/publication/PostsDocuments.jsx
@@ -167,7 +167,7 @@ const PostsDocuments = () => {
                     errorStatus={documentsErrors}
                     len={documents?.length} 
                     load={loadDocuments}
-                    message={'Auccun document pour l\'instant'}
+                    message={'Aucun document pour l\'instant'}
                 />
                 <NavigationPageCard
                   load={loadDocuments}
