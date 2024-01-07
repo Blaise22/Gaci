7dfa3823d00bac6b7d5a5e57f6271b014dc878fe
Blaise <blaisebhg3828@gmail.com>
2024-01-07 18:35:52 +0200

    change text style on data info container

--- src/components/extra/DataInfo.jsx
+++ src/components/extra/DataInfo.jsx
@@ -7,13 +7,13 @@ const DataInfo = ({errorStatus,len,load,message}) => {
             !load &&
             errorStatus ?
                 <div className="flex justify-center py-12 w-full  rounded lg bg-gray-100">
-                    Une erreur est survenue
+                    <span className='text-gray-600'>Une erreur est survenue</span>
                 </div>
             :
             !load &&
             len==0?
                 <div className="flex justify-center py-12 w-full  rounded lg bg-gray-100">
-                    {message?message:"Aucun resultat"}
+                    <span className='text-gray-600'>{message?message:"Aucun resultat"}</span>
                 </div>
             :null
         }
