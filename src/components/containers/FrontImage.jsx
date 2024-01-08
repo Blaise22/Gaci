e56566f44a80e597ef0c03e1bc02cc14233a65b6
Blaise <blaisebhg3828@gmail.com>
2024-01-08 13:39:05 +0200

    change home image

--- src/components/containers/FrontImage.jsx
+++ src/components/containers/FrontImage.jsx
@@ -1,17 +1,25 @@
 
 import React from 'react'
 import SearchContainer from './SearchContainer'
-
+import img from '../../assets/image_search_1599066129369.jpg'
 const FrontImage = () => {
     
   return (
     <>
-        <div className='pt-14 w-full bg-red-600' style={{'height':'400px'}}>
+        <div className='pt-14 px-4 sm:px-10 md:px-20   w-full bg-red-600' style={{
+            'height':'400px',
+            "backgroundImage":`url(${img})`,
+            "backgroundSize": "cover",
+            "backgroundRepeat": "no-repeat",
+            "backgroundPosition":"center",
+            "opacity":'0.6'
+            
+            }}>
             <div className='flex  justify-start'>
                
             </div>
             <div className='flex  pt-8'>
-              <h1 className='text-gray-600 text-xl font-semibold'>Groupe d'analystes et chercheurs en informatique.</h1>
+              <h1 className='text-blue-600 text-4xl font-semibold '>Groupe d'analystes et chercheurs en informatique.</h1>
             </div>
             <div className='flex justify-end'>
             <div className='lg:w-[30%] md:w-[50%] w-full mt-4'>
